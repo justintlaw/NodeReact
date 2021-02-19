14d79761423883552bf3591af63478acc5bfbfb4
justintlaw <justintlaw97@gmail.com>
2021-02-19 01:03:46 -0700

    more updates

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const express = require('express')
-const router = express.Router()
-const controllers = require('./../controllers/controllers')
-
-router.get('/say-something', controllers.saySomething)
-
-module.exports = router
